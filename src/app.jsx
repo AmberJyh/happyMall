13485bc6727a274267fa2239a2e835810aea11e2
amber <jiangyanhong@netsdl.com>
2019-06-25 17:35:12 +0800

    webpack配置

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css"
+import "./index.scss"
+import "../node_modules/font-awesome/css/font-awesome.min.css"
+
+ReactDOM.render(
+    <div>
+    <h1>Hello,world</h1>
+    <i class="fa fa-bicycle" aria-hidden="true"></i>
+    </div>,
+    document.getElementById('app'),
+);
