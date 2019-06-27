1326a3a9b4f6b37cc6164efc415b411842663d2c
amber <jiangyanhong@netsdl.com>
2019-06-27 17:36:23 +0800

    20190627

--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,16 @@ import "./index.css"
 import "./index.scss"
 import "../node_modules/font-awesome/css/font-awesome.min.css"
 
+// ReactDOM.render(
+//     <div>
+//     <h1>Hello,world</h1>
+//     <i className="fa fa-bicycle" aria-hidden="true"></i>
+//     </div>,
+//     document.getElementById('app'),
+// );
+
+let jsx=<div>jsx...</div>
 ReactDOM.render(
-    <div>
-    <h1>Hello,world</h1>
-    <i className="fa fa-bicycle" aria-hidden="true"></i>
-    </div>,
-    document.getElementById('app'),
-);
+    jsx,
+    document.getElementById('app')
+)
