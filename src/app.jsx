ab49f53b6b7982ef5f872943c170f5b74177c31f
amber <jiangyanhong@netsdl.com>
2019-06-28 14:48:40 +0800

    delete package-lock.json

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
 import "./index.css"
 import "./index.scss"
 import "../node_modules/font-awesome/css/font-awesome.min.css"
-import {Component} from React.Component
+
 
 // ReactDOM.render(
 //     <div>
@@ -17,8 +17,31 @@ let style={
     // fontSize:"30px"
 }
 let jsx=<div className="jsx" style={style}>jsx...</div>
+class Component extends Component{
+    constructor(){
+        super();
+        state:({
+            name:"Amber",
+            age:0
+        })
+    }
 
+    handleClick=()=>{
+        this.setState({
+            name:this.state.name+1
+        })
+    }
+    render(){
+        return(
+            <div>
+                <p>I am {this.state.name}</p>
+                <p>I am {this.state.age} years old!</p>
+                <button onClick={this.handleClick}>Add</button>
+            </div>
+        )
+    }
+}
 ReactDOM.render(
-    jsx,
+    <Component/>,
     document.getElementById('app')
 )
